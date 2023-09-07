3d3b3dee99a40942e6cb0bf05bfbe26ebb4d50d3
AtumaDavid <atumadavid35@gmail.com>
2023-09-07 08:52:52 +0100

    Next 13 Authentication with mongoDB

---
+++ app/api/register/route.js
@@ -0,0 +1,26 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import User from "@/models/user";
+
+export async function POST(req) {
+  try {
+    const { name, email, password } = await req.json();
+
+    // console.log("Name ", name);
+    // console.log("Email ", email);
+    // console.log("Password ", password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
+
+    return NextResponse.json({ message: "user registered" }, { status: 201 });
+  } catch (error) {
+    return (
+      NextResponse.json({
+        message: "an error occured while registering the user.",
+      }),
+      { status: 500 }
+    );
+  }
+}
